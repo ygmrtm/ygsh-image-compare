5a369d74550a04ccc29d8c0ac80fe4962862d0cb
Yog <yog.mortuum@gmail.com>
2023-12-05 14:50:07 -0600

    change to run under MAC

--- run_moveOldImages.sh
+++ run_moveOldImages.sh
@@ -3,8 +3,12 @@ source "${dir}"/moveOldImages.sh
 ######## Personal Cloud(Mega) - Keep 1 year
 ######## First Backup(Toshiba) last 8 years
 ######## Last Backup (WD) 8+ years
+main ~/"Google Drive/My Drive/En cas que mori (requiescat in pace)/_Fotos_" ~/"MEGA/yg.rwmind/Familia" 730 m
+main ~/"Google Drive/My Drive/En cas que mori (requiescat in pace)/_Fotos_" ~/"MEGA/yg.rwmind/Familia" 300 c
 main ~/"MEGA/yg.rwmind" "/Volumes/yg.diabolus/yg.rwmind" 365 m
-main ~/"Google Drive/My Drive/En cas que mori (requiescat in pace)/_Fotos_" "/Volumes/yg.diabolus/yg.rwmind/Familia" 730 m
+main ~/"MEGA/yg.rwmind" "/Volumes/yg.diabolus/yg.rwmind" 300 c
 main "/Volumes/yg.diabolus/yg.rwmind" "/Volumes/terra/Lalo/yg.rwmind" 2920 m
-main "/Volumes/yg.diabolus/yg.rwmind" "/Volumes/terra/Lalo/yg.rwmind" 365 c
-main ~/"Google Drive/My Drive/En cas que mori (requiescat in pace)/_Fotos_" ~/"MEGA/yg.rwmind/Familia" 365 c
+main "/Volumes/yg.diabolus/yg.rwmind" "/Volumes/terra/Lalo/yg.rwmind" 300 c
+
+tar -cvzf /Volumes/terra/Lalo/cloudBkp/dropbox.tar.gz ~/Dropbox
+tar -cvzf /Volumes/terra/Lalo/cloudBkp/mega.tar.gz ~/MEGA
